f08203bcf061b0bf4d1d35c6c69a712b3bd4cd6f
Steven Yang <steventanyang@gmail.com>
2025-01-11 14:23:58 -0500

    todo list

--- backfill_users.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public.users (id, username, email, balance_of_poo)
-SELECT 
-  au.id,
-  SPLIT_PART(au.email, '@', 1) as username, -- Takes everything before the @ symbol
-  au.email,
-  10000 as balance_of_poo
-FROM auth.users au
-LEFT JOIN public.users u ON au.id = u.id
-WHERE u.id IS NULL; 
